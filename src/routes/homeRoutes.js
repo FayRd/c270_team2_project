d7bbb5ffb700f20d462b69b33ea10527ea10ed08
FayRd <fayyadhrasid06@gmail.com>
2025-01-20 20:47:26 +0800

    Basic search function that returns a single recipe, and returns a 'No recipe Found' when there is none.

--- src/routes/homeRoutes.js
+++ src/routes/homeRoutes.js
@@ -2,10 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 const getRecipeList = require('../services/getRecipeList');
+const searchRecipe = require('../services/searchRecipe');
 
 router.get('/', async (req, res) => {
     const recipeList = await getRecipeList();
     res.render('index', { recipeList });
 });
+
+router.post('/search', async (req, res) => {
+    const query = req.body.query;
+    const recipe = await searchRecipe(query);
+    if (recipe === null) {
+        res.send('No matching recipe found.')
+    } else {
+        res.redirect(`/${recipe.idMeal}`)
+    }
+})
 
 module.exports = router;
---
+++ src/services/searchRecipe.js
@@ -0,0 +1,15 @@
+// Returns the most relevant match
+const searchRecipe = async (query) => {
+    console.log(query);
+    const api_url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`;
+    const res = await fetch(api_url);
+    const data = await res.json();
+    if (data.meals === null) {
+        return null;
+    }
+    console.log(data);
+    return data.meals[0];
+};
+
+module.exports = searchRecipe;
+
