5b3c9c9d79c610d41ad948d6c6d3574b93128de4
FayRd <fayyadhrasid06@gmail.com>
2025-01-20 22:23:08 +0800

    Updated parseInstructions.js

--- src/services/parseInstructions.js
+++ src/services/parseInstructions.js
@@ -1,6 +1,9 @@
 // Parse recipe instructions into an ordered list
 const parseInstructions = (recipe) => {
     const instructions = recipe.strInstructions;
+    if (!instructions) {
+        return null;
+    }
     let steps = instructions.split('.').map(s => s.trim()).filter(s => s !== '');
     steps = steps.filter(element => isNaN(element));
     return steps;
