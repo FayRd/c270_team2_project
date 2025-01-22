6a8ed7e9e30b8a858575af196b001f8ab6b41cf0
FayRd <fayyadhrasid06@gmail.com>
2025-01-22 12:39:30 +0800

    Modified config.json for DB config

--- src/routes/homeRoutes.js
+++ src/routes/homeRoutes.js
@@ -5,11 +5,13 @@ const { isAuthenticated } = require('../models/auth');
 const getRecipeList = require('../services/getRecipeList');
 const searchRecipe = require('../services/searchRecipe');
 
+// Render home page
 router.get('/', isAuthenticated, async (req, res) => {
     const recipeList = await getRecipeList();
     res.render('index', { recipeList });
 });
 
+// Search function
 router.post('/search', isAuthenticated, async (req, res) => {
     const query = req.body.query;
     const recipe = await searchRecipe(query);
