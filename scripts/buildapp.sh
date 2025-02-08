592c1791b61cab44bb4fdbb5822940e4922bb412
FayRd <fayyadhrasid06@gmail.com>
2025-02-08 19:14:05 +0800

    Updated buildapp.sh

--- scripts/buildapp.sh
+++ scripts/buildapp.sh
@@ -37,8 +37,8 @@ docker network create my-app-network
 docker run -d -p 3306:3306 --name my-mysql-container --network my-app-network -e MYSQL_ROOT_PASSWORD=123 fayrd/recipe_sql:1.0
 # Wait for MySQL to become ready
 echo "Waiting for MySQL to be ready..."
-until docker exec my-mysql-container mysqladmin ping -h"172.18.0.2" --silent; do
-    sleep 3
+until docker exec my-mysql-container mysqladmin ping -h"localhost" --silent; do
+    sleep 2
     echo "Waiting for MySQL..."
 done
 echo "MySQL is ready!"
